bbe545395bcebe970d27c379f946d1ab5a332650
Thomas Duran <samoht9@Thomass-MacBook-Pro-2.local>
2017-01-08 23:46:11 -0700

    Update love version to latest

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
   t.title = "Scrolling Shooter Tutorial" -- The title of the window the game is in
-  t.version = "0.9.1"    --the LOVE version this game was made for (string)
+  t.version = "0.10.2"    --the LOVE version this game was made for (string)
   t.window.width = 480
   t.window.height = 800
 
